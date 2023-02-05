fc56f552256077161e1a3120827a44c27ef3c3ed
Sofia Lakeeva <lakeeva.sofia@yandex.ru>
2023-02-05 16:59:40 +0300

    reformat a code

--- Laba4/src/Data.java
+++ Laba4/src/Data.java
@@ -8,32 +8,30 @@ public class Data {
     private ArrayList<String> L1 = new ArrayList<>();
     private ArrayList<String> L2 = new ArrayList<>();
 
-    public Data(String[] l1, String[] l2){
+    public Data(String[] l1, String[] l2) {
         Collections.addAll(L1, l1);
         Collections.addAll(L2, l2);
     }
 
-    private void write_to_file(boolean f){
-        try(FileWriter writer = new FileWriter("results.txt", false))
-        {
+    private void write_to_file(boolean f) {
+        try (FileWriter writer = new FileWriter("results.txt", false)) {
             String s1 = String.join(", ", L1);
             String s2 = String.join(", ", L2);
-            if (f){
+            if (f) {
                 writer.write(s1 + "  ->  " + s2 + "  ->  L1 входит в L2");
             } else {
                 writer.write(s1 + "  ->  " + s2 + "  ->  L1 не входит в L2");
             }
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Error: " + e);
         }
     }
 
-    public boolean is_in_list(){
+    public boolean is_in_list() {
         ListIterator<String> p2 = L2.listIterator();
-        while (p2.nextIndex()+L1.size()<=L2.size()){
-            int ind=p2.nextIndex();
-            if (L2.subList(ind, ind+L1.size()).equals(L1)){
+        while (p2.nextIndex() + L1.size() <= L2.size()) {
+            int ind = p2.nextIndex();
+            if (L2.subList(ind, ind + L1.size()).equals(L1)) {
                 write_to_file(true);
                 return true;
             }
